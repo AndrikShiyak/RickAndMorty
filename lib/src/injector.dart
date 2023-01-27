f30f7752fafa17761a9921aeb54ee44bb48d9059
Andriy Shiyak <andrikshiyak@gmail.com>
2023-01-27 15:40:47 +0200

    register characters_repository in injector

--- lib/src/injector.dart
+++ lib/src/injector.dart
@@ -2,6 +2,8 @@ import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:rick_and_morty_clean_practice/src/core/services/dio/dio_factory.dart';
 import 'package:rick_and_morty_clean_practice/src/core/utils/constants.dart';
+import 'package:rick_and_morty_clean_practice/src/data/repositories/characters_repository_impl.dart';
+import 'package:rick_and_morty_clean_practice/src/domain/repositories/characters_repository.dart';
 
 import 'data/data_sources/remote/characters_api.dart';
 
@@ -13,4 +15,7 @@ Future<void> initializeDependencies() async {
 
   injector.registerSingleton<CharactersApiService>(
       CharactersApiService(injector()));
+
+  injector.registerSingleton<CharactersRepository>(
+      CharactersRepositoryImpl(injector()));
 }
