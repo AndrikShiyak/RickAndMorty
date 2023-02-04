143ca3383aef8f24c0beb280375e6080313e3029
Andriy Shiyak <andrikshiyak@gmail.com>
2023-02-04 18:46:29 +0200

    refactor character_details_page, add key_value_widget

--- lib/src/presentation/pages/character_details_page.dart
+++ lib/src/presentation/pages/character_details_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:rick_and_morty_clean_practice/src/injector.dart';
 import 'package:rick_and_morty_clean_practice/src/presentation/blocs/remote_character_details/remote_character_details_bloc.dart';
+import '../../domain/entities/character.dart';
+import '../widgets/key_value_widget.dart';
 import 'state_page.dart';
 
 class CharacterDetailsPage extends StatelessWidget {
@@ -29,49 +31,84 @@ class CharacterDetailsView extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    // useEffect(() {
-    //   context.read<RemoteCharactersListBloc>().add(GetCharacter(characterId));
-
-    //   print('tratata tratata');
-    //   return () {};
-    // }, []);
-
     return BlocBuilder<RemoteCharacterDetailsBloc, RemoteCharacterDetailsState>(
       builder: (_, state) {
+        final Character? character = state.character;
+
         return StatePage(
           state: state.status,
           child: Scaffold(
             appBar: AppBar(
-              title: Text(state.character?.name ?? ''),
+              title: Text(character?.name ?? ''),
             ),
-            body: Column(
-              children: [
-                Image.network(state.character?.image ?? ''),
-                Expanded(
-                  child: Column(
-                    children: [
-                      Row(
-                        children: const [
-                          Text('key'),
-                          Text('value'),
-                        ],
-                      ),
-                      Row(
-                        children: const [
-                          Text('key'),
-                          Text('value'),
-                        ],
-                      ),
-                      Row(
-                        children: const [
-                          Text('key'),
-                          Text('value'),
-                        ],
-                      ),
-                    ],
+            body: SingleChildScrollView(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: Column(
+                children: [
+                  Image.network(
+                    character?.image ?? '',
+                    fit: BoxFit.fitWidth,
+                    width: double.infinity,
                   ),
-                ),
-              ],
+                  const SizedBox(height: 20),
+                  if (character != null)
+                    Column(
+                      children: [
+                        KeyValueWidget(
+                          keyText: 'Name: ',
+                          valueText: character.name,
+                        ),
+                        const SizedBox(height: 10),
+                        KeyValueWidget(
+                          keyText: 'Species: ',
+                          valueText: character.species,
+                        ),
+                        const SizedBox(height: 10),
+                        KeyValueWidget(
+                          keyText: 'Gender: ',
+                          valueText: character.gender,
+                        ),
+                        const SizedBox(height: 10),
+                        KeyValueWidget(
+                          keyText: 'Status: ',
+                          valueText: character.status,
+                        ),
+                        const SizedBox(height: 10),
+                        KeyValueWidget(
+                          keyText: 'Type: ',
+                          valueText: character.type,
+                        ),
+                        const SizedBox(height: 10),
+                        KeyValueWidget(
+                          keyText: 'Origin: ',
+                          valueText: character.origin.name,
+                        ),
+                        const SizedBox(height: 10),
+                        KeyValueWidget(
+                          keyText: 'Location: ',
+                          valueText: character.location.name,
+                        ),
+                        const SizedBox(height: 10),
+                        ListView(
+                          physics: const NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          children: [
+                            Text(
+                              'Episodes:',
+                              style: Theme.of(context).textTheme.headline6,
+                            ),
+                            const SizedBox(height: 10),
+                            for (var episode in character.episode)
+                              Padding(
+                                padding: const EdgeInsets.only(bottom: 5),
+                                child: Text(episode),
+                              ),
+                          ],
+                        ),
+                      ],
+                    ),
+                ],
+              ),
             ),
           ),
         );
---
+++ lib/src/presentation/widgets/key_value_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class KeyValueWidget extends HookWidget {
+  const KeyValueWidget({
+    super.key,
+    required this.keyText,
+    required this.valueText,
+  });
+
+  final String keyText;
+  final String valueText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          keyText,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        const SizedBox(width: 10),
+        Expanded(
+          child: Text(
+            valueText,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+      ],
+    );
+  }
+}
