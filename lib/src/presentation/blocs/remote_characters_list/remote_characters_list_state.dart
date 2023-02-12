5cfe2eca4f96f706061953b1cd21d3e56524bd73
Andriy Shiyak <andrikshiyak@gmail.com>
2023-02-13 01:47:20 +0200

    removed unused variable from states

--- lib/src/presentation/blocs/remote_characters_list/remote_characters_list_state.dart
+++ lib/src/presentation/blocs/remote_characters_list/remote_characters_list_state.dart
@@ -4,29 +4,25 @@ class RemoteCharactersListState extends Equatable {
   const RemoteCharactersListState({
     this.status = ViewState.loading,
     this.characters,
-    this.character,
     this.noMoreData = false,
   });
 
   final List<Character>? characters;
-  final Character? character;
   final ViewState status;
   final bool noMoreData;
 
   RemoteCharactersListState copyWith({
     List<Character>? characters,
-    Character? character,
     ViewState? status,
     bool? noMoreData,
   }) {
     return RemoteCharactersListState(
       characters: characters ?? this.characters,
-      character: character ?? this.character,
       status: status ?? this.status,
       noMoreData: noMoreData ?? this.noMoreData,
     );
   }
 
   @override
-  List<Object?> get props => [status, characters, character, noMoreData];
+  List<Object?> get props => [status, characters, noMoreData];
 }
--- lib/src/presentation/blocs/remote_episodes_list/remote_episodes_list_state.dart
+++ lib/src/presentation/blocs/remote_episodes_list/remote_episodes_list_state.dart
@@ -4,29 +4,25 @@ class RemoteEpisodesListState extends Equatable {
   const RemoteEpisodesListState({
     this.status = ViewState.loading,
     this.episodes,
-    this.episode,
     this.noMoreData = false,
   });
 
   final List<Episode>? episodes;
-  final Episode? episode;
   final ViewState status;
   final bool noMoreData;
 
   RemoteEpisodesListState copyWith({
     List<Episode>? episodes,
-    Episode? episode,
     ViewState? status,
     bool? noMoreData,
   }) {
     return RemoteEpisodesListState(
       episodes: episodes ?? this.episodes,
-      episode: episode ?? this.episode,
       status: status ?? this.status,
       noMoreData: noMoreData ?? this.noMoreData,
     );
   }
 
   @override
-  List<Object?> get props => [status, episodes, episode, noMoreData];
+  List<Object?> get props => [status, episodes, noMoreData];
 }
